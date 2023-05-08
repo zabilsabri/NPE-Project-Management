0896eeb8fcdb9be8574aa79903b1ee48fed2e8e7
erwinarif31 <muherwinarif31@gmail.com>
2023-05-08 10:33:18 +0800

    feat: add user_image field on user scheme

--- npe-project-management/database/migrations/2014_10_12_000000_create_users_table.php
+++ npe-project-management/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('divisi');
             $table->string('jabatan');
             $table->string('password');
+            $table->string('user_image');
             $table->rememberToken();
             $table->timestamps();
         });
