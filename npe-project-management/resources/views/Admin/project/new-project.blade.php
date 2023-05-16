07930f61dd88e49c224c5aa093150bd19e6aa10a
Ztzrk <ralif877@gmail.com>
2023-05-16 13:15:13 +0800

    detail-projek

--- npe-project-management/resources/views/Admin/project/new-project.blade.php
+++ npe-project-management/resources/views/Admin/project/new-project.blade.php
@@ -3,55 +3,64 @@
 @section('content')
 
 <div class="card">
-    <div class="card-body">
-                    <div class="form-row">
-                      <div class="form-group col-md-6">
-                        <label for="inputEmail4">Nama Projek</label>
-                        <input type="text" class="form-control" id="inputNama" placeholder="Nama">
-                      </div>
-                      <div class="form-group col-md-6">
-                        <label for="inputPassword4">Programmer</label>
-                        <input type="text" class="form-control" id="inputProgrammer" placeholder="Pilih Programmer">
-                      </div>
-                    </div>
-                    <div class="form-row">
-                      <div class="col-md-6">
-                      <div class="form-group">
-                        <label for="inputEmail4">Klien Projek</label>
-                        <input type="text" class="form-control" id="inputKlien" placeholder="Klien Projek">
-                      </div>
-                      <div class="form-group">
-                        <label for="inputEmail4">Tipe Projek</label>
-                        <input type="text" class="form-control" id="inputTipe" placeholder="Tipe Projek">
-                      </div>
-                    </div>
-                      <div class="form-group col-md-6">
-                        <label for="inputDetail" class="form-label">Detail Projek</label>
-                        <textarea class="form-control" id="inputDetail" rows="4" placeholder="Detail Projek"></textarea>
-                      </div>
-                    </div>
-                    <div class="form-row">
-                      <div class="form-group col-md-6">
-                      <label>Date Range Picker</label>
-                      <div class="input-group">
-                        <div class="input-group-prepend">
-                          <div class="input-group-text">
-                            <i class="fas fa-calendar"></i>
-                          </div>
-                        </div>
-                        <input type="text" class="form-control daterange-cus">
-                      </div>
-                    </div>
-                    </div>
-                    <div class="form-row">
-                      <div class="form-group col-md-6">
-                        <label for="inputState">Project Manager</label>
-                        <select id="inputState" class="form-control">
-                          <option selected="">Pilih Project Manager</option>
-                          <option>...</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div>
+  <div class="card-body">
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Nama Projek</label>
+        <input type="text" class="form-control" id="inputNama" placeholder="Nama">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputState">Employee</label>
+        <select id="inputState" class="form-control">
+          <option selected="">Pilih Employee</option>
+          <option>...</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="col-md-6">
+        <div class="form-group">
+          <label for="inputEmail4">Klien Projek</label>
+          <input type="text" class="form-control" id="inputKlien" placeholder="Klien Projek">
+        </div>
+        <div class="form-group">
+          <label for="inputEmail4">Tipe Projek</label>
+          <input type="text" class="form-control" id="inputTipe" placeholder="Tipe Projek">
+        </div>
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputDetail" class="form-label">Detail Projek</label>
+        <textarea class="form-control" id="inputDetail" rows="4" placeholder="Detail Projek"></textarea>
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label>Date Range Picker</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <div class="input-group-text">
+              <i class="fas fa-calendar"></i>
+            </div>
+          </div>
+          <input type="text" class="form-control daterange-cus">
+        </div>
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputState">Project Manager</label>
+        <select id="inputState" class="form-control">
+          <option selected="">Pilih Project Manager</option>
+          <option>...</option>
+        </select>
+      </div>
+
+    </div>
+    <div class="row float-right">
+      <a href="{{ route('project.admin') }}" class="btn btn-outline-danger mr-3">Batal</a>
+      <a href="" class="btn btn-primary mr-3">Buat Projek</a>
+    </div>
+  </div>
+
 </div>
 @endsection
