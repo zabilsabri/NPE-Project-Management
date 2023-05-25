f5688df024bc6e9bbb4a723d06d4e280902bb2ab
erwinarif31 <muherwinarif31@gmail.com>
2023-05-25 08:53:11 +0800

    feat: create, update, delete employee

--- npe-project-management/app/Http/Controllers/Admin/EmployeeController.php
+++ npe-project-management/app/Http/Controllers/Admin/EmployeeController.php
@@ -5,16 +5,72 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class EmployeeController extends Controller
 {
     public function index() {
-        $employees = User::orderByDesc('credit')->get();
+        $employees = User::orderByDesc('credit')->where('isAdmin', '0')->get();
 
         return view('Admin.employee.employee', compact('employees'));
     }
 
     public function create() {
         return view('Admin.employee.new-employee');
+    }
+    
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'nama' => ['required', 'max:255'],
+            'email' => ['required', 'email:dns', 'unique:users'],
+            'password' => ['required', 'min:6', 'max:225'],
+            'divisi' => ['required'],
+            'jabatan' => ['required'],
+            'nomorTelpon' => ['required'],
+        ]);
+
+        $validated['password'] = Hash::make($validated['password']);
+        /* dd($validated); */
+        User::create($validated);
+
+        return redirect(route('employee.admin'))->with('success', 'Data employee berhasil ditambahkan');
+    }
+
+    public function edit($id) {
+        $employee = User::find($id);
+
+        return view('Admin.employee.new-employee', compact('employee'));
+    }
+
+    public function update(Request $request) {
+        $validated = $request->validate([
+            'id' => ['required'],
+            'nama' => ['required', 'max:255'],
+            'email' => ['required', 'email:dns'],
+            'divisi' => ['required'],
+            'jabatan' => ['required'],
+            'nomorTelpon' => ['required'],
+        ]);
+
+        $user = User::find($validated['id']);
+        $user->nama = $validated['nama'];
+        $user->email = $validated['email'];
+        $user->divisi = $validated['divisi'];
+        $user->jabatan = $validated['jabatan'];
+        $user->nomorTelpon = $validated['nomorTelpon'];
+        
+        if (!is_null($request['password'])) {
+            $user->password = Hash::make($request['password']);
+        } 
+        
+        $user->save();
+
+        return redirect(route('employee.admin'))->with('success', 'Data employee berhasil diupdate');
+    }
+
+    public function destroy($id) {
+        User::find($id)->delete();
+
+        return redirect(route('employee.admin'))->with('success', 'Data employee berhasil dihapus');
     }
 }
--- npe-project-management/resources/views/Admin/employee/employee.blade.php
+++ npe-project-management/resources/views/Admin/employee/employee.blade.php
@@ -11,76 +11,83 @@
             <br>
             <small>Data Manejemen Projek NPE Digital</small>
         </div>
-        <a href="{{ route('employee-create.admin') }}"
-            class="btn btn-lg btn-outline-dark btn-icon icon-left float-right"><i class="fas fa-plus"></i>Tambah
+        <a href="{{ route('employee-create.admin') }}" class="btn btn-lg btn-outline-dark btn-icon icon-left float-right"><i class="fas fa-plus"></i>Tambah
             Karyawan</a>
     </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table display nowrap" style="width:100%" id="tableProject">
-                <thead>
-                    <tr>
-                        <th style="width: 10px" scope="col">No.</th>
-                        <th scope="col">Nama</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">No. HP</th>
-                        <th scope="col">Divisi</th>
-                        <th scope="col">Jabatan</th>
-                        <th scope="col">Skor Kredit</th>
-                        <th scope="col">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($employees as $employee)
-                    <tr class="project-row" data-id="">
-                        <th></th>
-                        <td>{{ $employee->nama }}</td>
-                        <td>{{ $employee->email }}</td>
-                        <td>{{ $employee->nomorTelpon }}</td>
-                        <td>{{ $employee->divisi }}</td>
-                        <td>{{ $employee->jabatan }}</td>
-                        <td>{{ $employee->credit }}</td>
-                        <td class="text-center">
-                            <!-- pensil -->
-                            <a href=""><img src="{{ asset('img/Admin/pensil.png') }}" alt="pensil"></a>
-                            <a href="#"><img src="{{ asset('img/Admin/zabil.png') }}" alt="pensil" data-toggle="modal"
-                                    data-target="#modal-hapus"></a>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
-        </div>
+    <div class="alert-container">
+    @if($msg = Session::get('success'))
+    <div class="alert alert-success alert-dismissible fade show m-3" role="alert">
+        {{ $msg }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
-    @endsection
-    <div class="modal fade" id="modal-hapus" aria-modal="true" role="dialog">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <div class="row">
-                        <span class="col align-self-center"><img src="{{ asset('img/Admin/icon.svg')}}" alt=""></span>
-                        <h4 class="modal-title ml-3 align-self-center">Hapus Data Karwayan</h4>
-                    </div>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">x</span>
-                    </button>
+    @endif
+    </div>
+    <div class="table-responsive p-3">
+        <table class="table display nowrap" style="width:100%" id="tableProject">
+            <thead>
+                <tr>
+                    <th style="width: 10px" scope="col">No.</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">No. HP</th>
+                    <th scope="col">Divisi</th>
+                    <th scope="col">Jabatan</th>
+                    <th scope="col">Skor Kredit</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($employees as $employee)
+                <tr class="project-row" data-id="" id="row-{{ $employee->id }}">
+                    <th></th>
+                    <td>{{ $employee->nama }}</td>
+                    <td>{{ $employee->email }}</td>
+                    <td>{{ $employee->nomorTelpon }}</td>
+                    <td>{{ $employee->divisi }}</td>
+                    <td>{{ $employee->jabatan }}</td>
+                    <td>{{ $employee->credit }}</td>
+                    <td class="text-center">
+                        <!-- pensil -->
+                        <a href="{{ route('employee-edit.admin', ['id' => $employee->id]) }}"><img src="{{ asset('img/Admin/pensil.png') }}" alt="pensil"></a>
+                        <a href="#"><img src="{{ asset('img/Admin/zabil.png') }}" class="btn-delete" alt="pensil" data-toggle="modal" data-target="#modal-hapus" data-id="{{ $employee->id }}"></a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+</div>
+@endsection
+<div class="modal fade" id="modal-hapus" aria-modal="true" role="dialog">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="row">
+                    <span class="col align-self-center"><img src="{{ asset('img/Admin/icon.svg')}}" alt=""></span>
+                    <h4 class="modal-title ml-3 align-self-center">Hapus Data Karwayan</h4>
                 </div>
-                <div class="modal-body">
-                    <p>Data yang sudah dihapus tidak bisa dikembalikan</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" id="btn-tambah">Hapus</button>
-                </div>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">x</span>
+                </button>
             </div>
-            <!-- /.modal-content -->
+            <div class="modal-body">
+                <p>Data yang sudah dihapus tidak bisa dikembalikan</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="btn-hapus">Hapus</button>
+            </div>
         </div>
+        <!-- /.modal-content -->
     </div>
+</div>
 
-    @section('script')
-    <script>
-        $(document).ready(function() {
+@section('script')
+<script>
+    $(document).ready(function() {
         var t = $('#tableProject').DataTable({
             retrieve: true,
             columnDefs: [{
@@ -103,6 +110,37 @@
                 this.data(i++);
             });
         }).draw();
+
+
+        let delete_id;
+        $('.btn-delete').on('click', function() {
+            delete_id = $(this).data('id');
+            console.log(delete_id);
+        });
+
+
+        $('#btn-hapus').on('click', function() {
+            $.ajax({
+                type: "POST",
+                data: {
+                    _token: "{{ csrf_token() }}",
+                    _method: "delete"
+                },
+                url: "/admin/employee/delete/" + delete_id,
+                success: function(response) {
+                    t.row($('#row-' + delete_id)).remove().draw();
+                    $('#modal-hapus').modal('hide');
+                    var alert_html = '<div class="alert alert-success alert-dismissible fade show m-3" role="alert">' +
+                        'Data berhasil dihapus' + 
+                        '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                        '<span aria-hidden="true">&times;</span>' + 
+                        '</button>'
+                        '</div>';
+                    $('.alert-container').html(alert_html);
+                },
+            });
+
+        });
     });
-    </script>
-    @endsection
+</script>
+@endsection
