7016dd8a902b6525e16dd508dbb7f71882b8b75a
Sofyan Pujas <gaero38@gmail.com>
2023-12-19 22:17:38 +0800

    feat: change driver to firefox

--- testing/main/test_example.py
+++ testing/main/test_example.py
@@ -1,7 +1,22 @@
 from pylenium.driver import Pylenium
+from pylenium.driver import WebDriverWait
+from pylenium import driver
 
-def test_google(py: Pylenium):
-    py.visit('https://google.com')
-    py.get('[name="q"]').type('puppies')
-    assert py.should().contain_title('puppies')
+
+def test_login(py: Pylenium):
+    py.visit('http://localhost:8000/')
+    py.get('input[name="email"]').type("admin@gmail.com")
+    py.get('input[name="password"]').type("password")
+    py.get('button[type="submit"]').click()
+    # assert py.should().have_url('http://localhost:8000/admin')
+    
+    
+    py.visit('http://localhost:8000/admin/project/create')
+    py.get('input[name="nama"]').type("Projek PPL JAGO")
+    py.get('input[name="klien"]').type("Mas-Mas NPE")
+    py.get('select[name="tipe"]').select_by_value("Web")
+    py.get('[name="deadline"]').type("2021-05-20")
+    
+    py.get('button[title="Silahkan Pilih PM Anda"]').click()
+    
     
